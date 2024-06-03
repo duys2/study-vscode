44a0e5f072c4d982f6f9497395328cf66d9f1064
Ays <minigun5@naver.com>
2024-06-03 11:15:39 +0900

    test commit

--- javaStudy/testFile.java
+++
@@ -1,9 +0,0 @@
-public class testFile {
-    public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i <= 5; i++) {
-            sum += i;
-        }
-        System.out.println("1부터 5까지의 합: " + sum);
-    }
-}
